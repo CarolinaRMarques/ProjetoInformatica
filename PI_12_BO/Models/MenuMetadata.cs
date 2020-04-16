7c63a0da66ae36fc0de46b89ccf69c7a51dff652
Carolina Marques <carolinaresendemarques@ua.pt>
2020-04-16 17:39:48 +0100

    Add Metadata for Menu to BackOffice\n Metadata used to simplifycalls

---
+++ PI_12_BO/Models/MenuMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PI_12_BO.Models
+{
+    public class MenuMetadata
+    {
+
+        public int ID { get; set; }
+        public string Nome { get; set; }
+    }
+}
